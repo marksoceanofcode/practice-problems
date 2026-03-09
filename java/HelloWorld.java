edd7c9e43391be6cec30645889a6dd2f5c8184c3
Mark <xenojs@outlook.com>
2026-03-09 11:43:00 -0700

    Hashmap practice

--- java/HelloWorld.java
+++ java/HelloWorld.java
@@ -1,5 +1,16 @@
+import java.util.HashMap;
+
 public class HelloWorld{
+
     public static void main(String[] args){
+        HashMap<String, String> myHashMap = new HashMap<String, String>();
+        myHashMap.put("England", "London");
+        myHashMap.put("California", "Sacramento");
+
         System.out.println("Hello World!");
+        System.out.println("My Hash Map:");
+        System.out.println(myHashMap);
+        System.out.println(myHashMap.get("California"));
+        System.out.println(myHashMap.keySet());
     }
 }
