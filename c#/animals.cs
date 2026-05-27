bbed63ce1d441a909026994767708309e8030072
Mark Angell <markangell85@gmail.com>
2026-05-27 15:27:56 -0700

    Additional updates

--- c#/animals.cs
+++ c#/animals.cs
@@ -1,6 +1,13 @@
 //Practice polymorphisim
 class Animal
 {
+  public string name;
+
+  public Animal(string animalName = "Generic Name")
+  {
+    name = animalName;
+  }
+
   public virtual void animalSound()
   {
     Console.WriteLine("Generic Animal Sound.");
@@ -9,13 +16,30 @@ class Animal
 
 class Dog : Animal
 {
+  public Dog(string dogName = "Unknown dog") : base(dogName)
+  {
+  }
   public override void animalSound(){
     Console.WriteLine("Rough rough!");
   }
 }
 
-class Cat: Animal
+class Dolphin : Animal
 {
+  public Dolphin(string dolphineName = "Unknown dolphin") : base(dolphineName)
+  {
+  }
+  public void swimmingSound()
+  {
+    Console.WriteLine("Slpoosh... splssh.");
+  }
+}
+
+class Cat : Animal
+{
+  public Cat(string catName = "Unknown cat") : base(catName) //Pass up the cat's name to the Animal "name" variable
+  {
+  }
   public override void animalSound()
   {
       Console.WriteLine("Meow.");
@@ -24,6 +48,10 @@ class Cat: Animal
 
 class Meerkat : Animal
 {
+  public Meerkat(string meerkatName = "Unknown meerkat") : base(meerkatName)
+  {
+  }
+
   public override void animalSound()
   {
     Console.WriteLine("Wrrr.");
@@ -37,7 +65,12 @@ class Program
     Animal meerkat1 = new Meerkat();
     meerkat1.animalSound();
 
-    Animal dolphine = new Animal();
-    dolphine.animalSound();
+    Cat cat1 = new Cat("Kibbles");
+    cat1.animalSound();
+    Console.WriteLine(cat1.name);
+
+    Dolphin dolphin = new Dolphin();
+    dolphin.animalSound();
+    dolphin.swimmingSound();
   }
 }
